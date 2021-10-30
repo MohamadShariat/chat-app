0c70d39c093965c6d43eacfadad3b36d5588fe5b
Mohammad Shariat <mohamadshariatahmadi@gmail.com>
2021-10-30 18:34:08 +0330

    the logout section is added

--- src/components/Chats.js
+++ src/components/Chats.js
@@ -1,4 +1,7 @@
 import React from "react";
+import { auth } from "../firebase";
+import { useHistory } from "react-router-dom";
+import { ChatEngine } from "react-chat-engine";
 
 //* Components
 import Navbar from "./Navbar";
@@ -7,9 +10,21 @@ import Navbar from "./Navbar";
 import styles from "./Chats.module.css";
 
 const Chats = () => {
+  const history = useHistory();
+
+  const logoutHandler = async () => {
+    await auth.signOut();
+    history.push("/");
+  };
   return (
     <div className={styles.container}>
-      <Navbar />
+      <Navbar logoutHandler={logoutHandler} />
+      <ChatEngine
+        height="calc(100vh - 50px)"
+        projectId="8555c401-ec56-453c-9f26-100fdf0d30fd"
+        userName="."
+        userSecret="."
+      />
     </div>
   );
 };
