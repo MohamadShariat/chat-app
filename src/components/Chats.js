748c41f1ef310a5aad2f9a23bd274c4eed22aa5f
Mohammad Shariat <mohamadshariatahmadi@gmail.com>
2021-11-04 20:30:27 +0330

    chat engine is added

--- src/components/Chats.js
+++ src/components/Chats.js
@@ -1,7 +1,8 @@
-import React from "react";
+import React, { useState, useEffect, useContext } from "react";
 import { auth } from "../firebase";
 import { useHistory } from "react-router-dom";
 import { ChatEngine } from "react-chat-engine";
+import axios from "axios";
 
 //* Components
 import Navbar from "./Navbar";
@@ -9,21 +10,75 @@ import Navbar from "./Navbar";
 //* Styles
 import styles from "./Chats.module.css";
 
+//* Context
+import { AuthContext } from "../contexts/AuthContextProvider";
+
 const Chats = () => {
+  const [loading, setLoading] = useState(true);
+  const user = useContext(AuthContext);
   const history = useHistory();
+
+  useEffect(() => {
+    if (!user) {
+      history.push("/");
+      return;
+    }
+
+    axios
+      .get("https://api.chatengine.io/users/me", {
+        headers: {
+          "project-id": "d0b2fdb7-e5ce-43d8-878a-ddb1664bdcc1",
+          "user-name": user.email,
+          "user-secret": user.uid,
+        },
+      })
+      .then(() => {
+        setLoading(false);
+      })
+      .catch(() => {
+        let formdata = new FormData();
+        formdata.append("email", user.email);
+        formdata.append("username", user.email);
+        formdata.append("secret", user.uid);
+        getFile(user.photoURL).then((avatar) => {
+          formdata.append("avatar", avatar, avatar.name);
+
+          axios
+            .post("https://api.chatengine.io/users/", formdata, {
+              headers: {
+                "private-key": "ae56f331-9c1e-495b-9bb3-de0742e0730d",
+              },
+            })
+            .then(() => {
+              setLoading(false).catch((error) => {
+                console.log(error);
+              });
+            });
+        });
+      });
+  }, [user, history]);
+
+  const getFile = async (url) => {
+    const response = await fetch(url);
+    const data = await response.blob();
+    return new File([data], "userPhoto.jpg", { type: "image/jpeg" });
+  };
 
   const logoutHandler = async () => {
     await auth.signOut();
     history.push("/");
   };
+
+  if (!user || loading) return "Loading...";
+
   return (
     <div className={styles.container}>
       <Navbar logoutHandler={logoutHandler} />
       <ChatEngine
         height="calc(100vh - 50px)"
-        projectId="8555c401-ec56-453c-9f26-100fdf0d30fd"
-        userName="."
-        userSecret="."
+        projectID="d0b2fdb7-e5ce-43d8-878a-ddb1664bdcc1"
+        userName={user.email}
+        userSecret={user.uid}
       />
     </div>
   );
